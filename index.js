a3f308bd36c213e78e53d72e3a134f5da3246e21
likunwu <likunwu@sailvan.com>
2023-02-01 11:21:43 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const puppeteer = require('puppeteer');
 
 async function run() {
-	// await pkg.open2('https://api.myip.com');
-	// let r = await pkg.runGetStock('https://api.myip.com',null);
-	// console.log(r);
 	const browser = await puppeteer.launch({headless:false});
 	const page = await browser.newPage();
 	await page.goto('https://baidu.com');
